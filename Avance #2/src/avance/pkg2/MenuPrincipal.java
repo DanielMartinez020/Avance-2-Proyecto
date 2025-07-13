c6eab959947f01d43c1cb05af866d14ac071ee3a
DanielMartinez020 <dm657882@gmail.com>
2025-07-12 22:23:42 -0600

    Mejorar la interfaz grafica, ademas de limpiar el codigo

--- Avance #2/src/avance/pkg2/MenuPrincipal.java
+++ Avance #2/src/avance/pkg2/MenuPrincipal.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package avance.pkg2;
 
-/**
- *
- * @author Daniel Martinez
- */
+
 public class MenuPrincipal extends javax.swing.JFrame {
 
     /**
